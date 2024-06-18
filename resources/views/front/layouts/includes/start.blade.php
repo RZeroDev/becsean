869eab7877d7a8c52540c6eb6d5be36e2b73e280
HadiCodeMaster <hadicodemaster@gmail.com>
2024-06-19 01:36:31 +0200

    front

---
+++ resources/views/front/layouts/includes/start.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from validthemes.net/site-template/agrica/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 18 Jun 2024 19:19:55 GMT -->
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Agrica - Organic Farm Agriculture Template">
+
+    <!-- ========== Page Title ========== -->
+    <title>Agrica - Organic Farm Agriculture Template</title>
+
+    <!-- ========== Favicon Icon ========== -->
+    <link rel="shortcut icon" href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/img/favicon.png')}}" type="image/x-icon">
+
+    <!-- ========== Start Stylesheet ========== -->
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/magnific-popup.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/flaticon-set.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/swiper-bundle.min.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/animate.min.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/validnavs.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/helper.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/unit-test.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/shop.css')}}" rel="stylesheet">
+    <link href="{{FrontHelper::getEnvFolder().asset('storage/front/assets/css/style.css')}}" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!-- ========== End Stylesheet ========== -->
+
+    <!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+</head>
+
+<body>
---
+++ resources/views/front/layouts/includes/start_header.blade.php
@@ -0,0 +1,42 @@
+<div class="top-bar-area top-bar-style-one bg-dark text-light">
+    <div class="container">
+        <div class="row align-center">
+            <div class="col-lg-8">
+                <ul class="item-flex">
+                    <li>
+                        <i class="fas fa-clock"></i> Opening Hours : Sunday- Friday, 08:00 am - 05:00pm
+                    </li>
+                    <li>
+                        <a href="tel:+4733378901"><i class="fas fa-phone-alt"></i> +4733378901</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4 text-end">
+                <div class="social">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
